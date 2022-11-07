d4890029ee8e56f83d176bd8190849e67f262282
appydog <94140371+appydog@users.noreply.github.com>
2022-11-07 20:46:58 +0000

    Test code to try and load CSS
    Added code to load the new CSS file into the page.
    
    I haven't bothered to set up a test environment yet, so can't test prior to committing... therefore, may contain errors!

--- loader.js
+++ loader.js
@@ -1,4 +1,24 @@
+///////////////////////////////
+// CONFIG
+// Constants defining the configuration.
+
+// The path to the page's CSS file.
+// TODO: Dynamically create the URL, relative to this JS file.
+var PATH_CSSFile = "https://appydog.github.io/TelehackProfile/happydog.css";
+
+///////////////////////////////
+// LOAD STYLESHEET
+
+var objLinkElement = document.createElement("link");
+objLinkElement.setAttribute("rel", "stylesheet");
+objLinkElement.setAttribute("type", "text/css");
+objLinkElement.setAttribute("href", PATH_CSSFile);
+
+document.getElementsByTagName("head")[0].appendChild(objLinkElement);
+
+///////////////////////////////
 // Proof of concept for Github hosting.
 // Will be replaced by something actually worthwhile, eventually.
+
 if (typeof console != "undefined")
 	console.log("Loader loaded.");
