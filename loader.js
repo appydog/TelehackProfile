c05f98338c68b91ddd63c3a8dcb288c2238ba773
Mark Clements <kewpie@kennel17.co.uk>
2022-11-09 13:57:28 +0000

    My first custom profile!
    A repectful hom*age to a great piece of internet history: Strong Bad's original computer, as seen on homestarrunner.com (https://homestarrunner.com/sbemails/1-some-kinda-robot)
    
    Cross-browser tested against modern Chrome, Firefox and Edge, plus reasonably old versions of Firefox and Edge.
    
    Use of SVG means it won't work on old IE versions, but may work on more recent IE (as yet untested).
    
    Technically, JavaScript is not required - all the styling is done via CSS with only a few enhancements (contrast control and the screen glare) added by JavaScript.  However, as the CSS file is currently loaded by JavaScript, this is a moot point.  I could improve appearance for people who browse without JS by also including the CSS file inclusion in my .plan file, but that would make it more crufty when viewing as text.  So, for now, JS is a requirement in order to see the enhanced page.

--- loader.js
+++ loader.js
@@ -17,8 +17,52 @@
 	document.getElementsByTagName("head")[0].appendChild(objLinkElement);
 
 ///////////////////////////////
-// Proof of concept for Github hosting.
-// Will be replaced by something actually worthwhile, eventually.
+// FUNCTIONAL CODE
 
-	if (typeof console != "undefined")
-		console.log("Loader loaded.");
+	var MIN_Contrast = 1;
+	var MAX_Contrast = 12;
+
+	var Contrast = 1;
+
+	function ContrastDown() {
+		AdjustContrast(-1);
+		return false;
+	}
+
+	function ContrastUp() {
+		AdjustContrast(1);
+		return false;
+	}
+
+	function AdjustContrast(Amount) {
+		Contrast = Contrast + Amount;
+
+		if (Contrast < MIN_Contrast)
+			Contrast = MIN_Contrast;
+		if (Contrast > MAX_Contrast)
+			Contrast = MAX_Contrast;
+
+		var Body = document.getElementsByTagName("body")[0];
+		if (Body.className.indexOf("Contrast") != -1) {
+			Body.className = Body.className.replace(/ Contrast\d+/, "");
+		}
+
+		Body.className += " Contrast" + Contrast;
+		console.log(Contrast, Body.className);
+	}
+
+	var objElement;
+
+	objElement = document.createElement('div');
+	objElement.id = "ScreenShine";
+	document.getElementsByTagName("body")[0].appendChild(objElement);
+
+	objElement = document.createElement('div');
+	objElement.id = "ContrastDown";
+	objElement.onclick = ContrastDown;
+	document.getElementsByTagName("body")[0].appendChild(objElement);
+
+	objElement = document.createElement('div');
+	objElement.id = "ContrastUp";
+	objElement.onclick = ContrastUp;
+	document.getElementsByTagName("body")[0].appendChild(objElement);
