b08b10af73cc53268f541a1c9ac5de14fce3c8db
appydog <94140371+appydog@users.noreply.github.com>
2022-11-09 13:50:50 +0000

    Fixed indentation in loader.js and use of leading white-space in happydog.css, as per my coding standards.
    No functional changes.

--- loader.js
+++ loader.js
@@ -4,21 +4,21 @@
 
 // The path to the page's CSS file.
 // TODO: Dynamically create the URL, relative to this JS file.
-var PATH_CSSFile = "https://appydog.github.io/TelehackProfile/happydog.css";
+	var PATH_CSSFile = "https://appydog.github.io/TelehackProfile/happydog.css";
 
 ///////////////////////////////
 // LOAD STYLESHEET
 
-var objLinkElement = document.createElement("link");
-objLinkElement.setAttribute("rel", "stylesheet");
-objLinkElement.setAttribute("type", "text/css");
-objLinkElement.setAttribute("href", PATH_CSSFile);
+	var objLinkElement = document.createElement("link");
+	objLinkElement.setAttribute("rel", "stylesheet");
+	objLinkElement.setAttribute("type", "text/css");
+	objLinkElement.setAttribute("href", PATH_CSSFile);
 
-document.getElementsByTagName("head")[0].appendChild(objLinkElement);
+	document.getElementsByTagName("head")[0].appendChild(objLinkElement);
 
 ///////////////////////////////
 // Proof of concept for Github hosting.
 // Will be replaced by something actually worthwhile, eventually.
 
-if (typeof console != "undefined")
-	console.log("Loader loaded.");
+	if (typeof console != "undefined")
+		console.log("Loader loaded.");
